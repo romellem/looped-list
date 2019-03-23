9a92a5f575b23b7e8e688a341a87d58de5bc96b9
romellem <romellem.git@gmail.com>
2019-03-22 22:36:02 -0500

    Adds in a basic test file

---
+++ test/index.js
@@ -0,0 +1,29 @@
+import { assert } from 'chai';
+import LoopedList, { LoopedListItem } from '../src';
+
+describe('Constructor Tests', () => {
+  it('should not initialize when constructed with no value', () => {
+    let list = new LoopedList();
+    assert.equal(list.head, undefined);
+  });
+
+  it('should initialize with a LoopedListItem when constructed with a value', () => {
+    let list = new LoopedList(1);
+    assert.equal(list.head instanceof LoopedListItem, true);
+    assert.equal(list.head.value, 1);
+  });
+
+  it('should allow for arrays to be passed', () => {
+    let list = new LoopedList([1, 2, 3]);
+    assert.equal(list.head.value, 1);
+
+    list.move(1);
+    assert.equal(list.head.value, 2);
+
+    list.move(1);
+    assert.equal(list.head.value, 3);
+
+    list.move(1);
+    assert.equal(list.head.value, 1);
+  });
+});
