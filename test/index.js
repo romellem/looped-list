4593cc4528f15073daebcd4d93e7d15ea2735a72
romellem <romellem.git@gmail.com>
2019-03-30 22:07:35 -0500

    Nest LoopedList tests

--- test/index.js
+++ test/index.js
@@ -1,45 +1,69 @@
 import assert from 'assert';
 import { LoopedList, LoopedListItem } from '../lib/umd/looped-list.js';
 
-describe('LoopedList `constructor` Tests', () => {
-    it('should not initialize when constructed with no value', () => {
-        let list = new LoopedList();
-        assert.strictEqual(list.head, undefined);
-    });
+describe('LoopedList Tests', () => {
+    describe('constructor', () => {
+        it('should not initialize when constructed with no value', () => {
+            let list = new LoopedList();
+            assert.strictEqual(list.head, undefined);
+        });
 
-    it('should initialize with a LoopedListItem when constructed with a value', () => {
-        let list = new LoopedList(1);
-        assert.strictEqual(list.head instanceof LoopedListItem, true);
-        assert.strictEqual(list.head.value, 1);
-    });
+        it('should initialize with a LoopedListItem when constructed with a value', () => {
+            let list = new LoopedList(1);
+            assert.strictEqual(list.head instanceof LoopedListItem, true);
+            assert.strictEqual(list.head.value, 1);
+        });
 
-    it('should allow for arrays to be passed', () => {
-        let list = new LoopedList([1, 2, 3]);
-        assert.strictEqual(list.head.value, 1);
+        it('should allow for arrays to be passed', () => {
+            let list = new LoopedList([1, 2, 3]);
+            assert.strictEqual(list.head.value, 1);
 
-        list.move(1);
-        assert.strictEqual(list.head.value, 2);
+            list.move(1);
+            assert.strictEqual(list.head.value, 2);
 
-        list.move(1);
-        assert.strictEqual(list.head.value, 3);
+            list.move(1);
+            assert.strictEqual(list.head.value, 3);
 
-        list.move(1);
-        assert.strictEqual(list.head.value, 1);
-    });
+            list.move(1);
+            assert.strictEqual(list.head.value, 1);
+        });
 
-    it('should throw an error when not used as a constructor', () => {
-        assert.throws(LoopedList);
+        it('should initialize with a LoopedListItem when constructed with a value', () => {
+            let list = new LoopedList(1);
+            assert.strictEqual(list.head instanceof LoopedListItem, true);
+            assert.strictEqual(list.head.value, 1);
+        });
+
+        it('should allow for arrays to be passed', () => {
+            let list = new LoopedList([1, 2, 3]);
+            assert.strictEqual(list.head.value, 1);
+
+            list.move(1);
+            assert.strictEqual(list.head.value, 2);
+
+            list.move(1);
+            assert.strictEqual(list.head.value, 3);
+
+            list.move(1);
+            assert.strictEqual(list.head.value, 1);
+        });
+
+        it('should throw an error when not used as a constructor', () => {
+            assert.throws(LoopedList);
+        });
     });
 });
 
-describe('LoopedList `move` Tests', () => {
-    it('should not tick forward when passed a move length less than 1', () => {
-        let list = new LoopedList([1, 2]);
+describe('LoopedList', () => {
+    describe('`move`', () => {
+        it('should not tick forward when passed a move length less than 1', () => {
+            let list = new LoopedList([1, 2]);
 
-        list.move(0);
-        assert.strictEqual(list.head.value, 1);
+            list.move(0);
+            assert.strictEqual(list.head.value, 1);
 
-        list.move(-1);
-        assert.strictEqual(list.head.value, 1);
+            list.move(-1);
+            assert.strictEqual(list.head.value, 1);
+        });
     });
 });
